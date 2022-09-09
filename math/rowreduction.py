daa6531e308cc5a148e934563a6055bd6d8d78ba
Burhan Rajgara <burhan.rajgara@gmail.com>
2022-09-09 18:39:10 -0400

    solve systems

--- math/rowreduction.py
+++ math/rowreduction.py
@@ -1,36 +1,10 @@
 import numpy as np
 
 
-def old_reduce(a: np.array):
-    b = np.copy(a)
-    b = b.astype(np.float64)
-
-    for i in range(min(np.size(b, 1), np.size(b, 0))):
-        if b[i, i] == 0:
-            continue
-        for j in range(i+1, np.size(b, 0)):
-            b[j] -= b[j, i]/b[i, i] * b[i]
-
-    for i in reversed(range(min(np.size(b, 1), np.size(b, 0)))):
-        if b[i, i] == 0:
-            continue
-        for j in reversed(range(i)):
-            b[j] -= b[j, i]/b[i, i] * b[i]
-
-    for i in range(min(np.size(b, 0), np.size(b, 1))):
-        if b[i, i] == 0:
-            continue
-        b[i] /= b[i, i]
-
-    return b
-
-
 def reduce(a: np.array):
-    b = np.copy(a)
-    b = b.astype(np.float64)
+    b = np.copy(a).astype(np.float64)
 
-    i = 0
-    j = 0
+    i, j = 0, 0
     while i < min(np.size(b, 0), np.size(b, 1)) and j < np.size(b, 0):
         k = j
         while b[k, i] == 0:
@@ -53,14 +27,47 @@ def reduce(a: np.array):
     return b
 
 
+def solveSystem(a):
+    b = reduce(a)
+    pivots = []
+    free = []
+    i = 0
+    for row in b:
+        while i < len(row) and row[i] == 0:
+            free.append(i)
+            i += 1
+        if i >= len(row):
+            break
+        if i == len(row) - 1:
+            return None
+        pivots.append(i)
+        i += 1
+    
+    m = -b[:,free]
+    m[:,-1] *= -1
+
+    soln = np.zeros((np.size(b,1)-1,len(free)))
+    for n, i in enumerate(pivots):
+        soln[i] = m[n]
+    
+    for n, i in enumerate(free[:-1]):
+        soln[i,n] = 1
+
+    return soln
+
+    
+
+
 def randMat():
     return np.random.randint(0, 20, (np.random.randint(1, 10), np.random.randint(1, 10)))
 
 
 A = np.random.randint(-10, 10, (3, 4))
 
-A = [[0,1,3,4],[1,0,3,3],[1,2,9,11]]
+A= np.array([[2,-2,1,-2,-2],[1,-1,-1,2,-1],[-1,1,2,-4,1],[3,-3,0,0,-3]])
 
 
 print(A)
 print(np.round(reduce(A), 1))
+
+print(np.round(solveSystem(A), 1))
